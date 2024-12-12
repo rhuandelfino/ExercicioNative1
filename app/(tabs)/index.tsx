d187af5da6244770d981ff30f39a6c18926916dd
rhuandelfino <127902637+rhuandelfino@users.noreply.github.com>
2024-12-12 16:17:27 -0300

    Add files via upload

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const YourApp = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+      }}>
+      <Text>Aqui é o Squad FuBox mano 😔👊</Text>
+    </View>
+  );
+};
+
+export default YourApp;
